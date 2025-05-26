72254318d530f4110a1d59fa97b3cd69b70325fa
Pessoal <edvaldopfernandes@gmail.com>
2025-05-25 23:00:19 -0300

    changes

--- samira/footer.php
+++ samira/footer.php
@@ -1,3 +1,12 @@
+<style>
+footer {
+	background: #EEEBE8;
+	text-align: center;
+	padding: 15px;
+	font-size: 14px;
+}
+</style>
+
 <footer>
     <p>&copy; 2025 - SC Nail Designer</p>
 </footer>
--- samira/head.php
+++ samira/head.php
@@ -1,3 +1,18 @@
+<style>
+html, body {
+	height: 100%;
+	margin: 0;
+	display: flex;
+	flex-direction: column;
+}
+
+.pagina {
+	flex: 1;
+	display: flex;
+	flex-direction: column;
+}
+</style>
+
 <head>
 	<meta charset="UTF-8">
 	<title>SC Nail Designer</title>
